70f416b70e99b821751aa28339e495060f682456
Iliya-creator <78891186+Iliya-creator@users.noreply.github.com>
2022-09-11 10:33:49 -0700

    ADD: update to JS Classes

---
+++ homeworkVideosFollowAlong/For Class 8/classes.js
@@ -0,0 +1,30 @@
+// A class is a blueprint for objects
+class User{
+    //instance properties -> data
+    username = ''
+    // constructor -> that is executed when an instance is created
+    constructor(){
+        console.log("Instance was created!");
+    }
+    //methods -> define logic to manipulate data
+}
+
+
+
+new User();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let userExample = {
+//     username: undefined
+// }
