056db8e4a94a9f6a3709a78039793d5ef8f4c851
Iliya-creator <78891186+Iliya-creator@users.noreply.github.com>
2022-09-19 16:28:28 -0700

    ADD

--- homeworkVideosFollowAlong/For Class 10/my-first-app/src/app/app.module.ts
+++ homeworkVideosFollowAlong/For Class 10/my-first-app/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
