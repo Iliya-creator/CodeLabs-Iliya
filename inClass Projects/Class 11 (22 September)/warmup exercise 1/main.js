d726b5929a0d319a4171eb6c5a3a0e2eb7db2802
Iliya-creator <78891186+Iliya-creator@users.noreply.github.com>
2022-09-22 17:44:51 -0700

    ADD corrections

--- inClass Projects/Class 11 (22 September)/warmup exercise 1/main.js
+++ inClass Projects/Class 11 (22 September)/warmup exercise 1/main.js
@@ -2,7 +2,15 @@
 // Evaluate the expression of a and b depending on what operator is (..see defined function below).
 
 function calculate(a, b, operator) {
-return (a + operator + b);
+    if (operator == "+"){
+        return a+b;
+    } else if (operator == "-"){
+        return a-b;
+    } else if (operator == "*"){
+        return a*b;
+    } else if (operator == "/"){
+        return a/b;
+    }
 }
 
 console.log(calculate(1,1, "+")); // 2
